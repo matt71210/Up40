2a3ca478bf2515303232bd1200b7ef329740d582
matt71210 <2r8r4kf64w@privaterelay.appleid.com>
2026-08-07 22:21:37 +0200

    Fix result page import resolution

--- app/result/page.tsx
+++ app/result/page.tsx
@@ -3,8 +3,8 @@
 import Link from 'next/link'
 import { useEffect, useState } from 'react'
 import { useRouter } from 'next/navigation'
-import { createClient } from '@/lib/supabase/client'
-import { getProgramAdjustment, type ProfileInput } from '@/lib/program'
+import { createClient } from '../../lib/supabase/client'
+import { getProgramAdjustment, type ProfileInput } from '../../lib/program'
 
 type Profile = ProfileInput & {
   id?: string
